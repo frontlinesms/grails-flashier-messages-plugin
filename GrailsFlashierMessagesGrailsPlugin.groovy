a5c82ab21d9e23285e05c7d34f4081b1cbc48d8a
Alex Anderson <alex@frontlinesms.com>
2013-06-11 12:54:47 +0300

    Plugin cleanup

--- GrailsFlashierMessagesGrailsPlugin.groovy
+++ GrailsFlashierMessagesGrailsPlugin.groovy
@@ -1,58 +1,21 @@
 class GrailsFlashierMessagesGrailsPlugin {
-	// the plugin version
-	def version = "0.1-SNAPSHOT"
-	// the version or versions of Grails the plugin is designed for
+	def version = '1.0-SNAPSHOT'
 	def grailsVersion = "2.0 > *"
-	// the other plugins this plugin depends on
 	def dependsOn = [:]
-	// resources that are excluded from plugin packaging
 	def pluginExcludes = [
-	"grails-app/views/error.gsp"
+		"grails-app/views/error.gsp"
 	]
 	def observe = ['controllers']
-
-	// TODO Fill in these fields
-	def title = "Grails Flashier Messages Plugin" // Headline display name of the plugin
-	def author = "FrontlineSMS"
+	def title = 'Grails Flashier Messages Plugin'
+	def author = 'Sitati Kituyi'
 	def authorEmail = ""
-	def description = '''\
-	Alternative to flash.message, that works between redirects and across multiple browser windows
-	'''
-
-	// URL to the plugin's documentation
+	def description = 'Alternative to flash.message, that works between redirects and across multiple browser windows'
 	def documentation = "http://grails.org/plugin/grails-flashier-messages"
-
-	// Extra (optional) plugin metadata
-
-	// License: one of 'APACHE', 'GPL2', 'GPL3'
-	//    def license = "APACHE"
-
-	// Details of company behind the plugin (if there is one)
-	//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-	// Any additional developers beyond the author specified above.
-	//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-	// Location of the plugin's issue tracker.
-	//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-	// Online location of the plugin's browseable source code.
-	//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
-
-	def doWithWebDescriptor = { xml ->
-		// TODO Implement additions to web.xml (optional), this event occurs before
-	}
-
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}
+	def license = "APACHE"
+	def organization = [name:"FrontlineSMS", url:"http://www.frontlinesms.com/" ]
 
         def doWithDynamicMethods = { ctx ->
 		applyDynamicMethods(application)
-	}
-
-	def doWithApplicationContext = { applicationContext ->
-		// TODO Implement post initialization spring config (optional)
 	}
 
 	def onChange = { event ->
@@ -61,20 +24,11 @@ class GrailsFlashierMessagesGrailsPlugin {
 
         void applyDynamicMethods(application) {
                 def svc = application.mainContext.oneTimeDataService
-
-                        application.controllerClasses.each { cc ->
-				cc.clazz.metaClass.setFlashMessage = { message ->
-					delegate.oneTimeData('message') { text = message }
-				}   
+		application.controllerClasses.each { cc ->
+			cc.clazz.metaClass.setFlashMessage = { message ->
+				delegate.oneTimeData('message') { text = message }
 			}
+		}
         }
-
-	def onConfigChange = { event ->
-		// TODO Implement code that is executed when the project configuration changes.
-		// The event is the same as for 'onChange'.
-	}
-
-	def onShutdown = { event ->
-		// TODO Implement code that is executed when the application shuts down (optional)
-	}
 }
+
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,37 +1,30 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-	// inherit Grails' default dependencies
-	inherits("global") {
-		// uncomment to disable ehcache
-		// excludes 'ehcache'
-	}
-	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	inherits 'global'
+	log 'warn'
 	repositories {
-		grailsCentral()
-		mavenLocal()
-		mavenRepo 'http://dev.frontlinesms.com/m2repo/'
-
 		grailsHome()
+		mavenLocal()
 		grailsCentral()
 		mavenCentral()
-		mavenRepo 'https://repo.springsource.org/repo'
+		mavenRepo 'http://dev.frontlinesms.com/m2repo/'
 	}
 	dependencies {
-		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-		// runtime 'mysql:mysql-connector-java:5.1.5'
 	}
 
 	plugins {
-		build(":tomcat:$grailsVersion",
-				":release:1.0.0") {
+		build ':release:2.2.1',
+				':rest-client-builder:1.0.3', {
 			export = false
 		}
+
+		test ':spock:0.7', {
+			exclude "spock-grails-support"
+		}
+		test ':hibernate:2.2.1', ':tomcat:2.2.1'
+
 		compile ":one-time-data:1.1-frontlinesms-SNAPSHOT"
 	}
 }
